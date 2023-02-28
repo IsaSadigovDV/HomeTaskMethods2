9f8d5dad011c3e186913caa2d6c0283cc3fd87de
IsaSadigovDV <isans@code.edu.az>
2023-02-28 14:11:49 +0400

    Added HomeTask Methods file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//Task 1
+
+
+void Finder(int[] numbers, int number)
+{
+	foreach (var item in numbers)
+	{
+		if (number == item)
+		{
+			Console.WriteLine("Var");
+			return;
+		}
+	}
+	Console.WriteLine("Yoxdu");
+
+}
+
+int[] arr = { 2, 4, 5 };
+Finder(arr, 4);
+
+// *******************************
+// Task 2
+
+int MaxFind(params int[] numbers)
+{
+	int Max = numbers[0];
+	for (int i = 0; i < numbers.Length; i++)
+	{
+		if (numbers[i] > Max)
+		{
+			Max = numbers[i];
+		}
+
+	}
+	return Max;
+}
+
+int[] a = { 2, 5, 6,3,9};
+Console.WriteLine(MaxFind(a));
